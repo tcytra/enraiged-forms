cb11bb3130d0b8f645efecd72517a37363225d2f
Todd Cytra <tcytra@gmail.com>
2025-10-19 17:06:56 -0700

    Repository: Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Enraiged Forms
+
+**Enraiged Forms provides form handling features for Laravel Enraiged projects.**
+
+> **Please Note:** This package is currently a work in progress and not yet ready for production use.
+
+
+## License
+
+Enraiged Forms is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ src/FormsServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Enraiged\Forms;
+
+// use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\ServiceProvider;
+
+class FormsServiceProvider extends ServiceProvider
+{
+    /**
+     *  Bootstrap the event services.
+     *
+     *  @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
